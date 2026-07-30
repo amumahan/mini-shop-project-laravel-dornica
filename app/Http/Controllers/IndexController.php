361749ee15dd66ef0e12f410f4c9c498b5374fbd
amumahan <amumahan4354@gmail.com>
2026-07-30 18:57:42 +0330

    feat(footer): load footer content from database

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\ProductStatus;
 use App\Models\Product;
 use App\Models\ProductCategory;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class IndexController
@@ -21,6 +22,7 @@ class IndexController
             ->orderByDesc('order_items_sum_qty')
             ->limit(5)
             ->get();
-        return view('index',compact('title','products','productCategories','bestSellingProduct'));
+        $setting = Setting::first();
+        return view('index',compact('title','products','productCategories','bestSellingProduct','setting'));
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -22,5 +24,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Paginator::useBootstrapFour();
+
+        View::composer('layouts.footer', function ($view) {
+            $setting = Setting::first();
+
+            $view->with('setting', $setting);
+        });
     }
 }
